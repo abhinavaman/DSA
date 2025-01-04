e2d2dfa653dfdff04929767ba4c72d159cf2e4c6
Your Name <your-email@example.com>
2025-01-05 02:11:50 +0530

    done

---
+++ patternn/pattern1.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+    int main(){
+
+        int n= 3;
+
+        int num = 1;
+
+        for (int i = 0; i <=n-1; i++)
+        {
+            for (int j = 0; j <=n-1; j++)
+            {
+               cout << num <<" ";
+               num++;
+            }
+            cout<<endl;
+        }
+        return 0;
+    }
