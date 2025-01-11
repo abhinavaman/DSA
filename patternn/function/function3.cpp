4d3ffc0ab359214f93274efa07c43faada3adc3d
Your Name <your-email@example.com>
2025-01-11 11:43:24 +0530

    done

---
+++ patternn/function/function3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sumofdigits(int num){
+    int digSum =0;
+    while (num>0)
+    {
+        int lastDig = num % 10;
+        num /=10;
+
+        digSum +=lastDig;
+
+    }
+    
+    return digSum;
+}
+
+int main(){
+    cout<<"sum = "<< sumofdigits(2356)<< endl;
+    return 0;
+}
